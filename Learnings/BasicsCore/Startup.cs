32774dc6fe135b3e4d9ca94f44676d5734ad9b7e
Anil Kumar Papena <APapena@slb.com>
2018-11-30 11:24:09 +0530

    Changes for the services

--- Learnings/BasicsCore/Startup.cs
+++ Learnings/BasicsCore/Startup.cs
@@ -25,6 +25,7 @@ namespace BasicsCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
